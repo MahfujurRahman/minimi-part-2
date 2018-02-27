4798e5490c6d97245620b11b73f444311c6b4bbe
Mahfujur Rahman <mahfujur705@gmail.com>
2018-02-28 00:44:58 +0600

    last commit
    all compilite

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,15 @@
         
         $('[data-toggle="tooltip"]').tooltip();
 
+        $(".treger-menu").on("click", function(){
+        	$(".offe-canvar-menu, .canvar-menu-overlay").addClass("active");
+        	return false;
+        })
+
+        $(".menu-close, .canvar-menu-overlay").on("click", function(){
+        	$(".offe-canvar-menu, .canvar-menu-overlay").removeClass("active");
+        })
+
 
 
         
